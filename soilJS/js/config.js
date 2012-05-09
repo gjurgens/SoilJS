d3a65937fe730463c1286fe5af08c6f67e64b56c
Gabriel Jurgens <gabriel@jurgens.com.ar>
2012-05-09 19:08:01 -0300

    Change path to correct folder

--- soilJS/js/config.js
+++ soilJS/js/config.js
@@ -1,9 +1,9 @@
 var require = {
 	paths : {
-		'jquery' : '/rjs/jsLib/jquery-1.7.2.min',
-		'soilJS' : '/rjs/soilJS/js',
-		'home'   : '/rjs/home/js',
-		'flights': '/rjs/flights/js'
+		'jquery' : '/SoilJS/jsLib/jquery-1.7.2.min',
+		'soilJS' : '/SoilJS/soilJS/js',
+		'home'   : '/SoilJS/home/js',
+		'flights': '/SoilJS/flights/js'
 	},
 	priority : [ "jquery", "soilJS/main"],
 	urlArgs : "bust=" + (new Date()).getTime()
