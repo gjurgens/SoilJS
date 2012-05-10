1d0b2e29b2532560919410c9b44034f0dc86fe2a
Gabriel Jurgens <gabriel@jurgens.com.ar>
2012-05-10 17:34:07 -0300

    moved sample structure to specific root folder

--- sampleDirStructure/soilJS/js-versioned/config.js
+++ sampleDirStructure/soilJS/js-versioned/config.js
@@ -1,1 +1,1 @@
-var require={paths:{jquery:"/SoilJS/jsLib/jquery-1.7.2.min",soilJS:"/SoilJS/soilJS/js",home:"/SoilJS/home/js",flights:"/SoilJS/flights/js"},priority:["jquery","soilJS/main"],urlArgs:"bust="+(new Date).getTime()}
+var require={paths:{jquery:"/SoilJS/sampleDirStructure/jsLib/jquery-1.7.2.min",soilJS:"/SoilJS/sampleDirStructure/soilJS/js",home:"/SoilJS/sampleDirStructure/home/js",flights:"/SoilJS/sampleDirStructure/flights/js"},priority:["jquery","soilJS/main"],urlArgs:"bust="+(new Date).getTime()}
