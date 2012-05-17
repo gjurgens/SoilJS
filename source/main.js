1cd5b3ff9f7c7bc687f3d8d4c708ca6a34aa64e0
Gabriel Jürgens <gabriel@GJMacBookPro.local>
2012-05-16 23:13:53 -0300

    jstestdriver separated files for dist and develop

--- source/main.js
+++ source/main.js
@@ -1,5 +1,4 @@
 define(["soilJS/helper"], function(helper){
-	console.log(helper);
 	helper.met();
 	return {prop: 1, k: helper.name};
 });
---
+++ test/requirejsTestHelper-dist.js
@@ -0,0 +1,13 @@
+var requirejsTestHelper = function(interval) {
+	//Defaul interval to renew context is 5 seconds
+	if(!interval || interval < 0) interval = 5000;
+	return require.config({
+		//Unique context generation for avoiding internal caching between tests		
+		context: "cont-" + Math.round((new Date()).getTime()/interval),
+		paths : {
+			'jquery' : '/test/dist/lib/jquery-1.7.2.min',
+			'soilJS' : '/test/dist'
+		}
+	});
+}
+
