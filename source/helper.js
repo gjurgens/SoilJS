ee4f2ffe9e9b54b2052cef116ed3d6c33e3e403f
Gabriel Jurgens <gabriel@jurgens.com.ar>
2012-06-01 18:13:37 -0300

    jslint

--- source/helper.js
+++ source/helper.js
@@ -1,11 +1,12 @@
-define(function(){
-	//ssssss.ssss();
-	return {
-		name: "helper", 
-		met:function(){return true;}, 
-		br: function(message) {
-			console.log("br: " + message);
-			//vvvvvvvv.aaaaaa();
-		}
-	};
-})
+define(function () {
+    "use strict";
+    //ssssss.ssss();
+    return {
+        name: "helper",
+        met: function () {return true; },
+        br: function (message) {
+            console.log("br: " + message);
+            //vvvvvvvv.aaaaaa();
+        }
+    };
+});
--- source/shieldDefine.js
+++ source/shieldDefine.js
@@ -1,72 +1,73 @@
-define(["soilJS/errorHandler"], function(errorHandler){
-	function shieldReturnedObject(object, module) {
-		var name, method;
-		
-		for (name in object) {
-			method = object[name];
-			if (typeof method == "function") {
-				object[name] = function(def, name) {
-					return function() {
-						try {
-							return def.apply(this, arguments);
-						} catch (ex) {
-							ex.message += "; module: '" + module  + "', method: '" + name + "'";
-							errorHandler.addToStack(ex.message);
-							throw ex;
-						}
-					};
-				}(method, name);
-			}
-		}
-		
-		object.soilJSShielded = true;
-	}
-	
-	if(!require.soilJSShielded) {
-		define = function(origDefine) {
-			return function() {
-				var callbackArgIndex = 2;
-				var name = "anonymous";
-				var deps = [];
-				
-				//Allow for anonymous functions
-				if (typeof arguments[0] !== 'string') {
-					callbackArgIndex--;
-				} else {
-					name = arguments[0];
-				}
-				
-				//This module may not have dependencies
-				if (!(arguments[callbackArgIndex - 1] instanceof Array)) {
-					callbackArgIndex--;
-				} else {
-					deps = arguments[callbackArgIndex - 1];
-				}
-				
-				
-				arguments[callbackArgIndex] = function(orig) {
-					return function() {
-						var i = 0;
-						for(arg in arguments) {
-							if(typeof arguments[arg] === "object" && (!arguments[arg].soilJSShielded)) {
-								shieldReturnedObject(arguments[arg], deps[i]);
-							}
-							i++;
-						};
-						try{
-							return orig.apply(this, arguments);				
-						} catch(e) {
-							e.message += "; module: '" + name + "'";
-							errorHandler.addToStack(e.message);
-							throw(e);				
-						}
-					}
-				}(arguments[callbackArgIndex]);
-				
-				//arguments[callbackArgIndex] = shield(arguments[callbackArgIndex], name, "module");
-				origDefine.apply(this, arguments);
-			}
-		}(define);
-	}
-	require.soilJSShielded = true;
-})
+define([ "soilJS/errorHandler" ], function(errorHandler) {
+    "use strict";
+    function shieldReturnedObject(object, module) {
+        var name, method;
+
+        for (name in object) {
+            method = object[name];
+            if (typeof method === "function") {
+                object[name] = function(def, name) {
+                    return function() {
+                        try {
+                            return def.apply(this, arguments);
+                        } catch (ex) {
+                            ex.message += "; module: '" + module
+                                    + "', method: '" + name + "'";
+                            errorHandler.addToStack(ex.message);
+                            throw ex;
+                        }
+                    };
+                }(method, name);
+            }
+        }
+
+        object.soilJSShielded = true;
+    }
+
+    if (!require.soilJSShielded) {
+        define = function(origDefine) {
+            return function() {
+                var callbackArgIndex = 2, name = "anonymous", deps = [];
+
+                // Allow for anonymous functions
+                if (typeof arguments[0] !== 'string') {
+                    callbackArgIndex--;
+                } else {
+                    name = arguments[0];
+                }
+
+                // This module may not have dependencies
+                if (!(arguments[callbackArgIndex - 1] instanceof Array)) {
+                    callbackArgIndex--;
+                } else {
+                    deps = arguments[callbackArgIndex - 1];
+                }
+
+                arguments[callbackArgIndex] = function(orig) {
+                    return function() {
+                        var i = 0;
+                        for (var arg in arguments) {
+                            if (typeof arguments[arg] === "object"
+                                    && (!arguments[arg].soilJSShielded)) {
+                                shieldReturnedObject(arguments[arg], deps[i]);
+                            }
+                            i++;
+                        };
+                        try {
+                            return orig.apply(this, arguments);
+                        } catch (e) {
+                            e.message += "; module: '" + name + "'";
+                            errorHandler.addToStack(e.message);
+                            throw (e);
+                        }
+                    }
+                }(arguments[callbackArgIndex]);
+
+                // arguments[callbackArgIndex] =
+                // shield(arguments[callbackArgIndex], name, "module");
+                origDefine.apply(this, arguments);
+            }
+        }(define);
+    }
+    require.soilJSShielded = true;
+});
